74795bd9cecaebafbd17e99bbe4bc909959f3695
Alex Kim <alexandre.jeonghwan.kim@gmail.com>
2026-07-03 12:23:57 +0200

    config with features

---
+++ src/config.py
@@ -0,0 +1,77 @@
+# config.py
+
+FEATURES ={
+    "surface_col" : [
+
+        "livable_surface",
+        "total_land_surface"
+    ],
+
+    "categorical_col" : [
+        "category",
+        "terrace_orientation",
+        "province",
+        "postal_code"
+    ],
+
+    "count_col" : [
+        "bedrooms", 
+        "bathrooms", 
+        "toilets"
+        
+    ],
+    "ordinal_col" :[
+        "epc_ordinal", 
+        "kitchen_level"
+        
+    ],
+    "distance_col":[
+        "supermarket_nearest_walk_m", 
+        "preschool_nearest_walk_m", 
+        "elementary_school_nearest_walk_m",
+        "nursery_nearest_walk_m", 
+        "bus_stop_nearest_walk_m", 
+        "high_school_nearest_walk_m",
+        "charging_station_nearest_drive_m", 
+        "train_station_nearest_walk_m"
+    ],
+
+   "binary_col" : [
+       "garden", "entry_phone", "alarm",
+    "solar_panels","air_conditioning", "vat","electrical_certificate", "swimming_pool", "fireplace",
+    "security_door","terrace","hammam_sauna_jacuzzi"
+   ],
+
+   #"geographical_col": ["longitude", "latitude"]
+    #"geographical_col": ["dist_nearest_big_city"]
+    
+"geographical_col": [
+    "dist_brussels",
+    "dist_antwerp",
+    "dist_ghent",
+    "dist_liege",
+    "dist_charleroi",
+    "dist_bruges",
+    "dist_namur",
+    "dist_leuven",
+    "dist_mons",
+    "dist_aalst",
+    "dist_hasselt",
+    "dist_mechelen",
+    "dist_sintniklaas",
+    "dist_lalouviere",
+    "dist_kortrijk",
+    "dist_ostend",
+    "dist_tournai",
+    "dist_genk",
+    "dist_roeselare",
+    "dist_mouscron",
+    "dist_verviers",
+    "dist_lokeren"
+]
+
+}
+# "garage" , "attic" , "domotica" , "heat_pump", "cellar","terrace","hammam_sauna_jacuzzi"
+# from count : "supermarket_count"
+# ordinal : "glazing_level"
+TARGET = "price"
