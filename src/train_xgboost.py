6ba6d6711127cb0477dcbfadb4e50e7045547d54
Alex Kim <alexandre.jeonghwan.kim@gmail.com>
2026-07-03 12:39:49 +0200

    Test for XGBOOST

---
+++ src/train_xgboost.py
@@ -0,0 +1,80 @@
+
+import pandas as pd
+import joblib
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_absolute_error
+from xgboost import XGBRegressor
+from sklearn.model_selection import cross_val_score
+from src.config import FEATURES, TARGET
+from src.preprocessing import clean_data, features_engineering
+from src.pipeline import build_pipeline
+from sklearn.model_selection import KFold
+
+
+def train_xgb_model(df):
+
+    df = clean_data(df)
+    df = features_engineering(df)
+
+
+    X = df[FEATURES["surface_col"] + FEATURES["binary_col"] + FEATURES["count_col"] + FEATURES["distance_col"]+ FEATURES["ordinal_col"]+ FEATURES["geographical_col"]]
+    y = df[TARGET]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    num_cols = X.columns.tolist()
+
+    pipeline = build_pipeline(
+      
+        model=XGBRegressor(n_estimators=800,
+                            max_depth=6,
+                            learning_rate=0.02,
+                            subsample=0.7,
+                            colsample_bytree=0.7,
+                            min_child_weight=3,
+                            reg_lambda=3,
+                            random_state=42
+                            ),
+        num_cols=num_cols,
+        scale=False
+    )
+
+    pipeline.fit(X_train, y_train)
+
+    
+       
+    y_train_pred= pipeline.predict(X_train)
+    y_pred= pipeline.predict(X_test)
+    
+
+    kf = KFold(n_splits=5, shuffle=True, random_state=42)
+
+    cv_scores = cross_val_score(
+        pipeline,
+        X,
+        y,
+        cv =kf,
+        scoring = "r2"
+    )
+
+    print("r2_train", r2_score(y_train, y_train_pred))
+    print("MAE_train:", mean_absolute_error(y_train, y_train_pred))
+
+
+    print("R2:", r2_score(y_test, y_pred))
+    print("MAE:", mean_absolute_error(y_test, y_pred))
+
+    print("CV R2 scores:", cv_scores)
+    print("Mean CV R2:", cv_scores.mean())
+
+    joblib.dump(pipeline, "model/model_xgboost.pkl")
+
+    return pipeline
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("data/listings_clean_duplicate_final.csv")
+    train_xgb_model(df)
